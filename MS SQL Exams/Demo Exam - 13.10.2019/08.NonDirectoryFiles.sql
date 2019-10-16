6232fe125b93c39a84ad84bfe69d4f8096bfdeea
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-10-16 14:38:34 +0300

    Refactored some queries

--- MS SQL Exams/Demo Exam - 13.10.2019/08.NonDirectoryFiles.sql
+++ MS SQL Exams/Demo Exam - 13.10.2019/08.NonDirectoryFiles.sql
@@ -1,6 +1,6 @@
   SELECT f.Id,
 	     f.Name,
-  	     STR(f.Size, LEN(f.Size), 2) + 'KB' AS Size
+  	     CONCAT(f.Size, 'KB') AS Size
     FROM Files f
    WHERE NOT EXISTS (SELECT ParentId 
 					   FROM Files
