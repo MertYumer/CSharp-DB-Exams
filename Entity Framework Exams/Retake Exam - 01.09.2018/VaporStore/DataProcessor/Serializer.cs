d7ec1f2c6dd074a431e80e78ece9e4d7afab9152
Mert Yumer <45640355+MertYumer@users.noreply.github.com>
2019-12-04 20:15:03 +0200

    Implemented one of the export methods

--- Entity Framework Exams/Retake Exam - 01.09.2018/VaporStore/DataProcessor/Serializer.cs
+++ Entity Framework Exams/Retake Exam - 01.09.2018/VaporStore/DataProcessor/Serializer.cs
@@ -1,14 +1,51 @@
 ﻿namespace VaporStore.DataProcessor
 {
 	using System;
-	using Data;
+	using System.Linq;
 
-	public static class Serializer
+	using Data;
+	using ExportDtos;
+    using Newtonsoft.Json;
+
+    public static class Serializer
 	{
 		public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
 		{
-			throw new NotImplementedException();
-		}
+            var genres = context
+                .Genres
+                .Where(g => genreNames.Contains(g.Name))
+                .OrderByDescending(g => g.Games.Sum(ga => ga.Purchases.Count))
+                .ThenBy(g => g.Id)
+                .ToArray();
+
+            var genreDtos = genres
+                .Select(g => new ExportGenreDto
+                {
+                    Id = g.Id,
+                    Genre = g.Name,
+                    Games = g.Games
+                        .OrderByDescending(ga => ga.Purchases.Count)
+                        .ThenBy(ga => ga.Id)
+                        .Select(ga => new ExportGameDto
+                        { 
+                            Id = ga.Id,
+                            Title = ga.Name,
+                            Developer = ga.Developer.Name,
+                            Tags = ga
+                                .GameTags
+                                .Select(gt => gt.Tag.Name)
+                                .Aggregate((i, j) => i + ", " + j),
+                            Players = ga.Purchases.Count
+                        })
+                        .ToArray(),
+
+                    TotalPlayers = g.Games.Sum(ga => ga.Purchases.Count)
+                });
+
+            var jsonResult = JsonConvert.SerializeObject(genreDtos, Newtonsoft.Json.Formatting.Indented);
+
+            return jsonResult;
+        }
 
 		public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
 		{
--- Entity Framework Exams/Retake Exam - 01.09.2018/VaporStore/StartUp.cs
+++ Entity Framework Exams/Retake Exam - 01.09.2018/VaporStore/StartUp.cs
@@ -21,7 +21,7 @@
 			var projectDir = GetProjectDirectory();
 
 			ImportEntities(context, projectDir + @"Datasets/", projectDir + @"ImportResults/");
-			ExportEntities(context, projectDir + @"ImportResults/");
+			ExportEntities(context, projectDir + @"ExportResults/");
 
 			using (var transaction = context.Database.BeginTransaction())
 			{
